97608d4313c086581e2b12846bcf1c437157aba8
chaithra-harshi <chaithrabn96@gmail.com>
2020-06-07 15:03:34 +0530

    [chaithra]refactoring files

---
+++ BookStoreApplication-Backend/src/main/java/com/bridgelabz/BookStoreApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreApplicationBackendApplication.class, args);
+	}
+
+}
