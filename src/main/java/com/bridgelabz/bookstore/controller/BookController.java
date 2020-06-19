7574820ab4395c973e0226bc7976b60446010377
chaithra-harshi <chaithrabn96@gmail.com>
2020-06-19 14:30:25 +0530

    [chaithra]Refactored: Book Controller

--- src/main/java/com/bridgelabz/bookstore/controller/BookController.java
+++ src/main/java/com/bridgelabz/bookstore/controller/BookController.java
@@ -3,10 +3,12 @@ package com.bridgelabz.bookstore.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.bookstore.model.Book;
@@ -14,26 +16,27 @@ import com.bridgelabz.bookstore.model.dto.BookDto;
 import com.bridgelabz.bookstore.service.BookService;
 
 @RestController
+@RequestMapping("/book")
 public class BookController {
 
 	@Autowired
 	private BookService bookservice;
 
-	@GetMapping(value = "/bookStoreApplication/{authorName}")
+	@GetMapping(value = "/{authorName}",produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Book> searchBookByAuthorName(@PathVariable("authorName") String authorName) {
 		List<Book> bookList = bookservice.findBookByAuthorName(authorName);
 		return bookList;
 
 	}
 
-	@GetMapping(value = "/bookStoreApplication/{bookName}")
+	@GetMapping(value = "/{bookName}",produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Book> searchBookByBookName(@PathVariable("bookName") String bookName) {
 		List<Book> bookList = bookservice.findBookByTitle(bookName);
 		return bookList;
 
 	}
 
-	@GetMapping(value = "/bookStoreApplication/getBooks")
+	@GetMapping(value = "/getBooks")
 	public List<Book> getAllBooks() {
 		List<Book> bookList = bookservice.findAllBook();
 		return bookList;
