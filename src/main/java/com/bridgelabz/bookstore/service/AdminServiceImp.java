2b9120cc2513d8cfbe6d319b1c143cbaaf3f705a
venkataganapathi <kandulaganesh0@gmail.com>
2020-06-24 15:57:33 +0530

    [Ganapathi] Refactored : Added role id in jwtVerify method

--- src/main/java/com/bridgelabz/bookstore/service/AdminServiceImp.java
+++ src/main/java/com/bridgelabz/bookstore/service/AdminServiceImp.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.bridgelabz.bookstore.constants.Constant;
 import com.bridgelabz.bookstore.exception.BookException;
 import com.bridgelabz.bookstore.model.Book;
+import com.bridgelabz.bookstore.model.Role;
 import com.bridgelabz.bookstore.model.User;
 import com.bridgelabz.bookstore.repo.BookRepo;
 import com.bridgelabz.bookstore.repo.RoleRepository;
@@ -18,24 +19,23 @@ import com.bridgelabz.bookstore.utils.MailTempletService;
 import com.bridgelabz.bookstore.utils.TokenUtility;
 
 @Service
-public class AdminServiceImp implements AdminService{
+public class AdminServiceImp implements AdminService {
 
 	@Autowired
 	RoleRepository roleRepository;
-	
+
 	@Autowired
 	BookRepo bookRepository;
-	
+
 	@Autowired
 	UserRepo userRepository;
-	
+
 	@Autowired
 	private MailTempletService mailTempletService;
-	
-	
+
 	@Autowired
 	private Environment environment;
-	
+
 	@Override
 	public List<User> getBuyers() {
 
@@ -50,52 +50,52 @@ public class AdminServiceImp implements AdminService{
 
 	@Override
 	public List<Book> getAllBooks() {
-		
+
 		return bookRepository.findAllBooks();
 	}
 
 	@Override
 	public List<Book> getBooksForVerification() {
-		
+
 		return bookRepository.getBooksForVerification();
 	}
 
 	@Override
 	public void bookVerification(Long bookId, Long sellerId, String verify) throws BookException {
-		Book book = bookRepository.getBookById(bookId).orElseThrow(()-> new BookException(Constant.BOOK_NOT_FOUND , Constant.NOT_FOUND_RESPONSE_CODE));
-		
-		User seller=userRepository.findByUserId(sellerId);
-		if(verify.equals("yes")) {
+		Book book = bookRepository.getBookById(bookId)
+				.orElseThrow(() -> new BookException(Constant.BOOK_NOT_FOUND, Constant.NOT_FOUND_RESPONSE_CODE));
+
+		User seller = userRepository.findByUserId(sellerId);
+		Role role = roleRepository.getRoleByName("SELLER");
+		if (verify.equals("yes")) {
 			book.setApproved(true);
 			bookRepository.save(book);
-			registerMail(seller, environment.getProperty("book-approval-template-path"));
-		}
-		else {
+			registerMail(seller, role, environment.getProperty("book-approval-template-path"));
+		} else {
 			book.setApproved(false);
-			book.setNoOfRejections(book.getNoOfRejections()+1);
-			if(book.getNoOfRejections()>2) {
+			book.setNoOfRejections(book.getNoOfRejections() + 1);
+			if (book.getNoOfRejections() > 2) {
 				bookRepository.deleteBook(book);
-				registerMail(seller, environment.getProperty("book-deletion-template-path"));
-			}
-			else {
-				
+				registerMail(seller, role, environment.getProperty("book-deletion-template-path"));
+			} else {
+
 				bookRepository.save(book);
-				registerMail(seller, environment.getProperty("book-rejection-template-path"));
+				registerMail(seller, role, environment.getProperty("book-rejection-template-path"));
 			}
 		}
-		
+
 	}
-	
-	public void registerMail(User user, String templet) {
-		String token = TokenUtility.verifyResponse(user.getId());
-		sendMail(user,token, templet);
+
+	public void registerMail(User user, Role role, String templet) {
+		String token = TokenUtility.verifyResponse(user.getId(), role.getRoleId());
+		sendMail(user, token, templet);
 	}
 
 	public void sendMail(User user, String token, String templet) {
 		try {
 			mailTempletService.getTemplate(user, token, templet);
 		} catch (IOException e) {
-			
+
 		}
 	}
 }
