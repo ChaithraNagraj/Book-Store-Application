fee2e558a3cd768050590cdb02f0f43ca9e666ae
datta235 <saidatta1432015@gmail.com>
2020-06-28 11:44:29 +0530

    [datta] Deleted: unwanted imports and extara spaces

--- src/main/java/com/bridgelabz/bookstore/service/CartServiceImpl.java
+++ src/main/java/com/bridgelabz/bookstore/service/CartServiceImpl.java
@@ -31,16 +31,9 @@ public class CartServiceImpl implements CartService {
 	public boolean addtocart(String token, long bookId) {
 		
 		Long userId= (Long) JwtValidate.decodeJWT(token).get("userId");
-		
-//		Optional<Book> book = bookRepo.getBookById(bookId);
-		
-//		Long bookid=book.get().getBookId();
-		
 		Book bookk= bookRepo.findByBookId(bookId);
-		
 		 List<Book> cartBooks = new ArrayList<>();
 		 cartBooks.add(bookk);
-		
 		User user=userRepo.findByUserId(userId);
 		long userid=user.getId();
 		Cart cart=new Cart();
