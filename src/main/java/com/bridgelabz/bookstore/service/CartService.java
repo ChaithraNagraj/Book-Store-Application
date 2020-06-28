151a9af8ea9fa3779bb592e9b0f781472d332b54
datta235 <saidatta1432015@gmail.com>
2020-06-28 14:12:59 +0530

    [datta] Added: abstract method to removed book from cart

--- src/main/java/com/bridgelabz/bookstore/service/CartService.java
+++ src/main/java/com/bridgelabz/bookstore/service/CartService.java
@@ -3,5 +3,7 @@ package com.bridgelabz.bookstore.service;
 public interface CartService {
 
 	boolean addtocart(String token, long bookId);
+	boolean removeBookFromCart(String token, long bookId);
+	
 
 }
