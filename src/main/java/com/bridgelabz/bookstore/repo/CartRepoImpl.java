83c15db64fbd4439c9c43ded95336646fab052c9
datta235 <saidatta1432015@gmail.com>
2020-06-28 11:49:37 +0530

    [datta] resolved merge conflicts

--- src/main/java/com/bridgelabz/bookstore/repo/CartRepoImpl.java
+++ src/main/java/com/bridgelabz/bookstore/repo/CartRepoImpl.java
@@ -1,19 +1,18 @@
 package com.bridgelabz.bookstore.repo;
 
-import javax.transaction.Transactional;
-
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.bridgelabz.bookstore.model.Cart;
+
 @Repository
-@Transactional
+
 public class CartRepoImpl implements CartRepo {
 
 	@Autowired
 	private SessionFactory sessionFactory;
-	
+
 	public boolean saveToCart(Cart cart) {
 		sessionFactory.getCurrentSession().saveOrUpdate(cart);
 		return true;
