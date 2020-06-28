92f93489a3df0e965a71db9d09ed025bd410da8c
datta235 <saidatta1432015@gmail.com>
2020-06-28 14:13:26 +0530

    [datta] Added: implementation for  method to removed book from cart

--- src/main/java/com/bridgelabz/bookstore/service/CartServiceImpl.java
+++ src/main/java/com/bridgelabz/bookstore/service/CartServiceImpl.java
@@ -2,12 +2,14 @@ package com.bridgelabz.bookstore.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import com.bridgelabz.bookstore.constants.Constant;
+import com.bridgelabz.bookstore.exception.BookNotFoundException;
 import com.bridgelabz.bookstore.model.Book;
 import com.bridgelabz.bookstore.model.Cart;
 import com.bridgelabz.bookstore.model.User;
@@ -15,35 +17,51 @@ import com.bridgelabz.bookstore.repo.BookRepo;
 import com.bridgelabz.bookstore.repo.CartRepo;
 import com.bridgelabz.bookstore.repo.UserRepo;
 import com.bridgelabz.bookstore.utils.JwtValidate;
+import com.bridgelabz.bookstore.utils.TokenUtility;
 
 @Service
 @Component
 public class CartServiceImpl implements CartService {
+
+	@Autowired
+	private UserRepo userRepo;
+	@Autowired
+	private BookRepo bookRepo;
+	@Autowired
+	private CartRepo cartRepo;
 	
 	@Autowired
-	UserRepo userRepo;
-	@Autowired
-	BookRepo bookRepo;
-	@Autowired
-	CartRepo cartRepo;
+	private TokenUtility tokenUtility;
 
 	@Override
 	public boolean addtocart(String token, long bookId) {
-		
-		Long userId= (Long) JwtValidate.decodeJWT(token).get("userId");
-		Book bookk= bookRepo.findByBookId(bookId);
-		 List<Book> cartBooks = new ArrayList<>();
-		 cartBooks.add(bookk);
-		User user=userRepo.findByUserId(userId);
-		long userid=user.getId();
-		Cart cart=new Cart();
-		cart.setBookId(cartBooks);
-		cart.setUserId(user);
-		boolean a=cartRepo.saveToCart(cart);
-		if(a==true) {
+
+		Long userId = (Long) JwtValidate.decodeJWT(token).get("userId");
+		Book bookk = bookRepo.findByBookId(bookId);
+		List<Book> cartBooks = new ArrayList<>();
+		cartBooks.add(bookk);
+		User user = userRepo.findByUserId(userId);
+		long userid = user.getId();
+		Cart cart = new Cart();
+		cart.setBooks(cartBooks);
+		cart.setUser(user);
+		boolean a = cartRepo.saveToCart(cart);
+		if (a == true) {
 			return true;
 		}
-		return false;		
+		return false;
+	}
+
+	@Override
+	public boolean removeBookFromCart(String token, long bookId) {
+		User buyer = tokenUtility.authentication(token, Constant.ROLE_AS_BUYER);
+		List<Book> booksInCart = buyer.getUserCart().getBooks();
+		Book bookToBeRemoved=booksInCart.stream().filter(books -> books.getBookId() == bookId).findAny().orElseThrow(() -> {
+			throw new BookNotFoundException(Constant.BOOK_NOT_FOUND);
+		});
+		booksInCart.remove(bookToBeRemoved);
+		buyer.getUserCart().setBooks(booksInCart);
+		return cartRepo.saveToCart(buyer.getUserCart());
 	}
 
 }
