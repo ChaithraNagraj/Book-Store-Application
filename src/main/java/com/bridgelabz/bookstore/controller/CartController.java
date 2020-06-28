db926c7cff1f2dd32fa18548dc6abc0154348520
datta235 <saidatta1432015@gmail.com>
2020-06-28 14:10:14 +0530

    [datta] Added: api to remove the book from cart

--- src/main/java/com/bridgelabz/bookstore/controller/CartController.java
+++ src/main/java/com/bridgelabz/bookstore/controller/CartController.java
@@ -1,31 +1,22 @@
 package com.bridgelabz.bookstore.controller;
 
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.bookstore.constants.Constant;
-import com.bridgelabz.bookstore.model.Book;
 import com.bridgelabz.bookstore.response.Response;
 import com.bridgelabz.bookstore.service.CartService;
 
-import io.swagger.annotations.Api;
-
 @RestController
-@RequestMapping(value = { "/cart" })
+@RequestMapping(value = "/cart" )
 public class CartController {
 	
 	public CartController() {
@@ -43,6 +34,15 @@ public class CartController {
 		} else
 			return ResponseEntity.status(HttpStatus.OK).body(new Response(Constant.BOOK_ADD_TO_CART_FAILED, Constant.BAD_REQUEST_RESPONSE_CODE));
 
+	}
+	@DeleteMapping("/removeFromCart{/bookId}")
+	public ResponseEntity<Response> removeFromCart(@RequestHeader String token,@PathVariable long bookId){
+		boolean status = cartService.removeBookFromCart(token, bookId);
+		if(status) {
+			return ResponseEntity.status(HttpStatus.OK).body(new Response(Constant.BOOK_REMOVED_FROM_CART,Constant.OK_RESPONSE_CODE));
+		}
+		else
+			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new Response(Constant.BOOK_REMOVAL_FROM_CART_FAILED, Constant.BAD_REQUEST_RESPONSE_CODE));
 	}
 
 	
