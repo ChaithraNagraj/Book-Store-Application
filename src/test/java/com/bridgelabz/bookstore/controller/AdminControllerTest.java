692fc7e6379a3914e060ac92a45d7a75d554ad63
pallavi <pallavi2207@gmail.com>
2020-07-01 01:48:07 +0530

    [pallavi] Added: mockitoTesting of adminController

--- src/test/java/com/bridgelabz/bookstore/controller/AdminControllerTest.java
+++ src/test/java/com/bridgelabz/bookstore/controller/AdminControllerTest.java
@@ -3,12 +3,10 @@ package com.bridgelabz.bookstore.controller;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import javax.servlet.ServletContext;
-
-import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -16,33 +14,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.bridgelabz.bookstore.controller.UserController;
 import com.bridgelabz.bookstore.model.dto.LoginDTO;
-import com.bridgelabz.bookstore.model.dto.RegistrationDTO;
-import com.bridgelabz.bookstore.repo.UserRepo;
-import com.bridgelabz.bookstore.service.UserService;
+import com.bridgelabz.bookstore.service.AdminService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(UserController.class)
-
-public class UserControllerTest {
+@WebMvcTest(AdminController.class)
+class AdminControllerTest {
 	
 	@Autowired
 	MockMvc mockMvc;
 	
-
 	@MockBean
-	UserService service;
-	@MockBean
-	UserRepo userRepository;
+	AdminService adminService;
 	
 	@Autowired
 	private WebApplicationContext context;
@@ -54,36 +44,37 @@ public class UserControllerTest {
 				.build();
 		
 	}
-	
-	@Test
-	public void loginTest() throws Exception{
-		
-		mockMvc.perform(post("/users/login")
-		.content(asJsonString(new LoginDTO("pallavikumari2207@gmail.com","S_1tringa",2l)))
-		.contentType(MediaType.APPLICATION_JSON)
-		.accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk());
-	}
-	
-	@Test
-	public void userLoginVerification() throws Exception{
-		
-		mockMvc.perform(get("/users/verify")
-			.header("token", "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInJvbGVJZCI6MiwiaWF0IjoxNTkzNDU3ODQwLCJzdWIiOiJhdXRoZW50aWNhdGlvbiIsImlzcyI6IkJyaWRnZWxhYnoiLCJleHAiOjI0NTc0NTc4NDB9.HMHnoMRXV-cMnoZch2lDxofHOn9jVrgO9So9-12vVQw")	
-		.accept(MediaType.APPLICATION_JSON))
-	
-	    .andDo(print())
-	    .andExpect(status()
-	    .isOk());
-	}
 
 	@Test
-	final void testRegister() throws Exception {
-		//fail("Not yet implemented");	
-		mockMvc.perform(post("/users/register")
-				.content(asJsonString(new RegistrationDTO("Pallavi Kumari","S_1tringa","pallavikumari2207@gmail.com","A_1tring","2", "9122449097")))
-				
+	final void testGetSellersForVerification() throws Exception {
+		
+		mockMvc.perform(get("/admin/getSellersForVerification")
+				.header("token", "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInJvbGVJZCI6MiwiaWF0IjoxNTkzNDU3ODQwLCJzdWIiOiJhdXRoZW50aWNhdGlvbiIsImlzcyI6IkJyaWRnZWxhYnoiLCJleHAiOjI0NTc0NTc4NDB9.HMHnoMRXV-cMnoZch2lDxofHOn9jVrgO9So9-12vVQw")
 				.contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk());
+				.accept(MediaType.APPLICATION_JSON)).andDo(print())
+		.andExpect(status().isOk());
+		
+	}
+	
+	@Test
+	final void testgetBooksForVerification() throws Exception {
+		
+		mockMvc.perform(get("/admin/getBooksForVerification/{sellerId}","2")
+				.param("sellerId", "2")
+				.header("token", "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInJvbGVJZCI6MiwiaWF0IjoxNTkzNDU3ODQwLCJzdWIiOiJhdXRoZW50aWNhdGlvbiIsImlzcyI6IkJyaWRnZWxhYnoiLCJleHAiOjI0NTc0NTc4NDB9.HMHnoMRXV-cMnoZch2lDxofHOn9jVrgO9So9-12vVQw")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)).andDo(print())
+		.andExpect(status().isOk());
+	}
+	
+	@Test
+	final void testbookVerification() throws Exception {
+		
+		mockMvc.perform(put("/admin/bookVerification/{bookId}/{sellerId}/{verify}","2","2","true")
+				.header("token", "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInJvbGVJZCI6MiwiaWF0IjoxNTkzNDU3ODQwLCJzdWIiOiJhdXRoZW50aWNhdGlvbiIsImlzcyI6IkJyaWRnZWxhYnoiLCJleHAiOjI0NTc0NTc4NDB9.HMHnoMRXV-cMnoZch2lDxofHOn9jVrgO9So9-12vVQw")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)).andDo(print())
+		.andExpect(status().isOk());
 	}
 	
 	public static String asJsonString(final Object obj) {
