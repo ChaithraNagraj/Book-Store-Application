2f2f598fdd3d8414d41df308093d35235c9c0f57
pallavi <pallavi2207@gmail.com>
2020-06-30 23:52:44 +0530

    [pallavi] Added: mockTesting of userController

--- src/test/java/com/bridgelabz/bookstore/controller/UserControllerTest.java
+++ src/test/java/com/bridgelabz/bookstore/controller/UserControllerTest.java
@@ -1,42 +1,97 @@
 package com.bridgelabz.bookstore.controller;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.servlet.ServletContext;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import com.bridgelabz.bookstore.model.User;
+import com.bridgelabz.bookstore.controller.UserController;
+import com.bridgelabz.bookstore.model.dto.LoginDTO;
+import com.bridgelabz.bookstore.model.dto.RegistrationDTO;
+import com.bridgelabz.bookstore.repo.UserRepo;
 import com.bridgelabz.bookstore.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-class UserControllerTest {
 
-	@InjectMocks
-	UserController Usercontroller;
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
 
-	@Mock
+public class UserControllerTest {
+	
+	@Autowired
+	MockMvc mockMvc;
+	
+
+	@MockBean
 	UserService service;
-	User user = new User();
+	@MockBean
+	UserRepo userRepository;
+	
+	@Autowired
+	private WebApplicationContext context;
 
 	@BeforeEach
 	void setUp() throws Exception {
-
-		MockitoAnnotations.initMocks(this);
-		user = new User();
-		user.setEmail("email@email.com");
-		user.setId(1l);
-		user.setImageUrl("imageUrl");
-		user.setMobileNumber("1234567890");
-		user.setName("pallavi");
-		user.setUserName("Wxcvbn");
-		user.setUserStatus(false);
-		user.setVerify(false);
+		mockMvc = MockMvcBuilders
+				.webAppContextSetup(context)
+				.build();
+		
+	}
+	
+	@Test
+	public void loginTest() throws Exception{
+		
+		mockMvc.perform(post("/users/login")
+		.content(asJsonString(new LoginDTO("pallavikumari2207@gmail.com","S_1tringa",2l)))
+		.contentType(MediaType.APPLICATION_JSON)
+		.accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk());
+	}
+	
+	@Test
+	public void userLoginVerification() throws Exception{
+		
+		mockMvc.perform(get("/users/verify")
+			.header("token", "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInJvbGVJZCI6MiwiaWF0IjoxNTkzNDU3ODQwLCJzdWIiOiJhdXRoZW50aWNhdGlvbiIsImlzcyI6IkJyaWRnZWxhYnoiLCJleHAiOjI0NTc0NTc4NDB9.HMHnoMRXV-cMnoZch2lDxofHOn9jVrgO9So9-12vVQw")	
+		.accept(MediaType.APPLICATION_JSON))
+	
+	    .andDo(print())
+	    .andExpect(status()
+	    		.isOk());
 	}
 
-//	@Test
-//	final void testGetUserById() {
-//		when(service.findById(1l)).thenReturn(user);
-//		   Usercontroller.getUserById(1l);
-//		 assertEquals("pallavi", user.getName());
-//	}
+	@Test
+	final void testRegister() throws Exception {
+		//fail("Not yet implemented");	
+		mockMvc.perform(post("/users/register")
+				.content(asJsonString(new RegistrationDTO("Pallavi Kumari","S_1tringa","pallavikumari2207@gmail.com","A_1tring","2", "9122449097")))
+				
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk());
+	}
+	
+	public static String asJsonString(final Object obj) {
+	    try {
+	        return new ObjectMapper().writeValueAsString(obj);
+	    } catch (Exception e) {
+	        throw new RuntimeException(e);
+	    }
+	}
 
 }
--- src/test/java/com/bridgelabz/bookstore/service/UserServiceImpTest.java
+++ src/test/java/com/bridgelabz/bookstore/service/UserServiceImpTest.java
@@ -35,15 +35,14 @@ class UserServiceImpTest {
 
 	UserServiceImp service;
 
-	@Spy
+	@Mock
 	UserRepo repo;
 	User user = new User();
 
 	@Mock
 	RoleRepositoryImp roleRepository;
 
-	@Mock
-	UserRepo userRepo;
+
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -51,6 +50,7 @@ class UserServiceImpTest {
 
 		user = new User();
 		user.setEmail("email@email.com");
+		user.setName("pallavi");
 		user.setId(1l);
 		user.setImageUrl("imageUrl");
 		user.setMobileNumber("1234567890");
@@ -60,117 +60,69 @@ class UserServiceImpTest {
 
 	@Test
 	final void testFindById() {
-		User user = new User();
-		user.setEmail("email@email.com");
-		user.setId(1l);
-
-		user.setMobileNumber("1234567890");
-		user.setName("pallavi");
-		user.setUserName("Wxcvbn");
-		user.setUserStatus(false);
-		user.setVerify(false);
-	}
-
-
-//	@Test
-//	final void testFindById() {
-//		when(repo.findByUserId(1l)).thenReturn(user);
-//		service.findById(1l);
-
-//		 assertEquals("pallavi", user.getName());
-//	}
-//
-//	@Test
-//	final void testGetUser() {
-//		List<User> userlist=new ArrayList<> ();
-//		userlist.add(user);
-//		when(repo.getUser()).thenReturn(userlist);
-//		service.getUser();
-//		assertEquals("pallavi", user.getName());
-//	}
-//	
-//	@Test
-//	void testExpectedExceptionGetUser() {
-//		List<User> userlist=new ArrayList<> ();
-//		when(repo.getUser()).thenReturn(userlist);
-//		if(userlist.isEmpty())
-//		{
-//	  Assertions.assertThrows(UserNotFoundException.class, () -> {
-//		  service.getUser();
-//	  });
-//		}
-//	}
-	
-//	@Test
-//  void deleteUserByIdTest() {
-//	  long  doseId=42;
-//
-//      // perform the call
-//      service.deleteUserById(doseId);
-//
-//      // verify the mocks
-//      verify(repo,times(1)).delete((long) ((int)doseId));
-//  }
-	@Test
-	  void VerifyTest() throws UserException {
-		Role role=new Role();
-		role.setRole("seller");
-		role.setRoleId(2l);
-		List<User> userlist=new ArrayList<> ();
-		userlist.add(user);
-		role.setUser(userlist);
-//		long id=1;
-//		int rid=2;
-		
-		//when(roleRepository.getRoleById((int) rid)).thenReturn(role);
-		// role=roleRepository.getRoleById((int) rid);
-		// when(repo.findByUserId(1l)).thenReturn(user);
-		//User idAvailable=repo.findByUserId(1l);
-		//System.out.println("rolename"+idAvailable.getName());
-		
-		//idAvailable.setVerify(false);
-		//System.out.println("idAvailable"+idAvailable.isVerify());
-		//perform call
-		
-	//when(service.verify("eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInJvbGVJZCI6MiwiaWF0IjoxNTkzNDU3ODQwLCJzdWIiOiJhdXRoZW50aWNhdGlvbiIsImlzcyI6IkJyaWRnZWxhYnoiLCJleHAiOjI0NTc0NTc4NDB9.HMHnoMRXV-cMnoZch2lDxofHOn9jVrgO9So9-12vVQw")).thenReturn(true);
-		service.verify("eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInJvbGVJZCI6MiwiaWF0IjoxNTkzNDU3ODQwLCJzdWIiOiJhdXRoZW50aWNhdGlvbiIsImlzcyI6IkJyaWRnZWxhYnoiLCJleHAiOjI0NTc0NTc4NDB9.HMHnoMRXV-cMnoZch2lDxofHOn9jVrgO9So9-12vVQw");
-		Long Id = Long.valueOf((Integer) JwtValidate.decodeJWT("eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInJvbGVJZCI6MiwiaWF0IjoxNTkzNDU3ODQwLCJzdWIiOiJhdXRoZW50aWNhdGlvbiIsImlzcyI6IkJyaWRnZWxhYnoiLCJleHAiOjI0NTc0NTc4NDB9.HMHnoMRXV-cMnoZch2lDxofHOn9jVrgO9So9-12vVQw").get("userId"));
-		long roleId = Long.valueOf((Integer) JwtValidate.decodeJWT("eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInJvbGVJZCI6MiwiaWF0IjoxNTkzNDU3ODQwLCJzdWIiOiJhdXRoZW50aWNhdGlvbiIsImlzcyI6IkJyaWRnZWxhYnoiLCJleHAiOjI0NTc0NTc4NDB9.HMHnoMRXV-cMnoZch2lDxofHOn9jVrgO9So9-12vVQw").get("roleId"));
-		when(roleRepository.getRoleById((int) roleId)).thenReturn(role);
-		User idAvailable=repo.findByUserId(Id);
-		System.out.println("rolename"+idAvailable.getName());
-		idAvailable.setVerify(false);
-		
-		verify(repo, times(1)).verify(idAvailable.getId());
 		when(repo.findByUserId(1l)).thenReturn(user);
-	
-		
+		service.findById(1l);
+
+		 assertEquals("pallavi", user.getName());
 	}
-  
-
-	
-
 
 	@Test
 	final void testGetUser() {
-		List<User> userlist = new ArrayList<>();
+		List<User> userlist=new ArrayList<> ();
 		userlist.add(user);
 		when(repo.getUser()).thenReturn(userlist);
 		service.getUser();
 		assertEquals("pallavi", user.getName());
 	}
-
+	
 	@Test
 	void testExpectedExceptionGetUser() {
-		List<User> userlist = new ArrayList<>();
+		List<User> userlist=new ArrayList<> ();
 		when(repo.getUser()).thenReturn(userlist);
-		if (userlist.isEmpty()) {
-			Assertions.assertThrows(UserNotFoundException.class, () -> {
-				service.getUser();
-			});
+		if(userlist.isEmpty())
+		{
+	  Assertions.assertThrows(UserNotFoundException.class, () -> {
+		  service.getUser();
+	  });
 		}
 	}
+	
 
+//	@Test
+//	  void VerifyTest() throws UserException {
+//		Role role=new Role();
+//		role.setRole("seller");
+//		role.setRoleId(2l);
+//		List<User> userlist=new ArrayList<> ();
+//		userlist.add(user);
+//		role.setUser(userlist);
+////		long id=1;
+////		int rid=2;
+//		
+//		//when(roleRepository.getRoleById((int) rid)).thenReturn(role);
+//		// role=roleRepository.getRoleById((int) rid);
+//		// when(repo.findByUserId(1l)).thenReturn(user);
+//		//User idAvailable=repo.findByUserId(1l);
+//		//System.out.println("rolename"+idAvailable.getName());
+//		
+//		//idAvailable.setVerify(false);
+//		//System.out.println("idAvailable"+idAvailable.isVerify());
+//		//perform call
+//		
+//	//when(service.verify("eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInJvbGVJZCI6MiwiaWF0IjoxNTkzNDU3ODQwLCJzdWIiOiJhdXRoZW50aWNhdGlvbiIsImlzcyI6IkJyaWRnZWxhYnoiLCJleHAiOjI0NTc0NTc4NDB9.HMHnoMRXV-cMnoZch2lDxofHOn9jVrgO9So9-12vVQw")).thenReturn(true);
+//		service.verify("eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInJvbGVJZCI6MiwiaWF0IjoxNTkzNDU3ODQwLCJzdWIiOiJhdXRoZW50aWNhdGlvbiIsImlzcyI6IkJyaWRnZWxhYnoiLCJleHAiOjI0NTc0NTc4NDB9.HMHnoMRXV-cMnoZch2lDxofHOn9jVrgO9So9-12vVQw");
+//		Long Id = Long.valueOf((Integer) JwtValidate.decodeJWT("eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInJvbGVJZCI6MiwiaWF0IjoxNTkzNDU3ODQwLCJzdWIiOiJhdXRoZW50aWNhdGlvbiIsImlzcyI6IkJyaWRnZWxhYnoiLCJleHAiOjI0NTc0NTc4NDB9.HMHnoMRXV-cMnoZch2lDxofHOn9jVrgO9So9-12vVQw").get("userId"));
+//		long roleId = Long.valueOf((Integer) JwtValidate.decodeJWT("eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInJvbGVJZCI6MiwiaWF0IjoxNTkzNDU3ODQwLCJzdWIiOiJhdXRoZW50aWNhdGlvbiIsImlzcyI6IkJyaWRnZWxhYnoiLCJleHAiOjI0NTc0NTc4NDB9.HMHnoMRXV-cMnoZch2lDxofHOn9jVrgO9So9-12vVQw").get("roleId"));
+//		when(roleRepository.getRoleById((int) roleId)).thenReturn(role);
+//		User idAvailable=repo.findByUserId(Id);
+//		System.out.println("rolename"+idAvailable.getName());
+//		idAvailable.setVerify(false);
+//		
+//		verify(repo, times(1)).verify(idAvailable.getId());
+//		when(repo.findByUserId(1l)).thenReturn(user);
+//		
+//	}
+  
 	@Test
 	void deleteUserByIdTest() {
 		long doseId = 42;
@@ -180,32 +132,6 @@ class UserServiceImpTest {
 
 		// verify the mocks
 		verify(repo, times(1)).delete((long) ((int) doseId));
-	}
-
-
-	@Test
-	final void testgetUser() {
-
-		User user = new User();
-		user.setEmail("email@email.com");
-		user.setId(1l);
-
-		user.setMobileNumber("1234567890");
-		user.setName("pallavi");
-
-		user.setUserName("Wxcvbn");
-		user.setUserStatus(false);
-		user.setVerify(false);
-		user.setRegistrationDateTime(null);
-		user.setSellerBooks(null);
-		user.setRoleList(null);
-		user.setVerify(false);
-
-		List<User> ListOfUsr = new ArrayList<>();
-		ListOfUsr.add(user);
-		when(service.getUser()).thenReturn(ListOfUsr);
-		assertEquals("email@email.com", ListOfUsr.get(0).getEmail());
-
 	}
 
 
