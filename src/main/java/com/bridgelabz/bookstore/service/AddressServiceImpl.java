d5cc8e510c5527932bfcc2dd62d25ff5832ccade
Vinod-21 <vinodtitu1@gmail.com>
2020-07-08 11:27:42 +0530

    [vinod] Refactored:address

--- src/main/java/com/bridgelabz/bookstore/service/AddressServiceImpl.java
+++ src/main/java/com/bridgelabz/bookstore/service/AddressServiceImpl.java
@@ -1,6 +1,5 @@
 package com.bridgelabz.bookstore.service;
 
-
 import javax.transaction.Transactional;
 
 import org.springframework.beans.BeanUtils;
@@ -19,72 +18,57 @@ import com.bridgelabz.bookstore.utils.TokenUtility;
 @Service
 @Transactional
 public class AddressServiceImpl implements AddressService {
-	
+
 	public AddressServiceImpl() {
 		System.out.println("constructor of AddressServiceImpl is started workingg");
 	}
+
 	@Autowired
 	UserRepo userRepo;
 	@Autowired
 	AddressRepo addressRepo;
 	@Autowired
 	private TokenUtility tokenUtility;
+
 	@Override
 	public Address addAddress(AddressDTO address, String token) {
-		System.out.println("Adress service impl started");
-//		Long id = generate.parseJWT(token);
 		User user = tokenUtility.authentication(token, Constant.ROLE_AS_BUYER);
-		long userId=user.getId();
-//		System.out.println("printing the user");
-//		System.out.println(user);
-//		cart.setUser(buyer);
-		System.out.println("cmng from swagger---->"+ " "+address.getaddressType());
-		String type=address.getaddressType();
-		Address add2=addressRepo.findAddressByType(type, user.getId());
-		Address add=new Address();
-		
-		
-//				System.out.println("getting frm database---->"+" "+add2.getAddressType());
-				
-				if(add2!=null)
-				{
-					//do nothing as address already exist for this user
-					return add2;
-				}else {
-					
-					BeanUtils.copyProperties(address,add);
-					add.setUser(user);
-					user.getAddress().add(add);
-					addressRepo.save(add);
-					return add;
-				}
-		
-		
-			
- 		}
-		//throw new AddressTypeExistsException(add2.getAddressType() +" "+ "already exists");
+		long userId = user.getId();
+		String type = address.getaddressType();
+		Address add2 = addressRepo.findAddressByType(type, user.getId());
+		Address add = new Address();
+		if (add2 != null) {
+			add2.setAddress(address.getAddress());
+			add2.setAddressType(address.getaddressType());
+			add2.setCity(address.getCity());
+			add2.setLandmark(address.getLandmark());
+			add2.setLocality(address.getLocality());
+			add2.setName(address.getName());
+			add2.setPhoneNumber(address.getPhoneNumber());
+			add2.setPincode(address.getPincode());
+			add2.setUser(user);
+			user.getAddress().add(add2);
+			addressRepo.save(add2);
+			return add2;
+		} else {
+			BeanUtils.copyProperties(address, add);
+			add.setUser(user);
+			user.getAddress().add(add);
+			addressRepo.save(add);
+			return add;
+		}
 
-		
-
-
-//		System.out.println(add.getAddress()+"->"+add.getCity());
-////		Users userdetails = userRepo.findById(id)
-////				.orElseThrow(null);
-//		user.getAddress().add(add);
-//		return   addressRepository.save(add);
-		
+	}
 	
+
 	@Override
 	public Address getAddressByType(String addressType, String token) {
-//		Long id = generate.parseJWT(token);
 		User user = tokenUtility.authentication(token, Constant.ROLE_AS_BUYER);
-	    long userId=user.getId();
-
-		Address address=addressRepo.findAddressByType(addressType,userId);
+		long userId = user.getId();
+		Address address = addressRepo.findAddressByType(addressType, userId);
 		System.out.println("printing address from addressServiceimpl");
 		System.out.println(address);
 		return address;
 	}
 
 }
-
