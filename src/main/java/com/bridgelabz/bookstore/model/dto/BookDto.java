b1f31656ac38943283fce62cd2deca28c7743056
pallavi <pallavi2207@gmail.com>
2020-07-09 17:50:53 +0530

    [pallavi]: added AdminServiceTesting

--- src/main/java/com/bridgelabz/bookstore/model/dto/BookDto.java
+++ src/main/java/com/bridgelabz/bookstore/model/dto/BookDto.java
@@ -27,71 +27,57 @@ public class BookDto {
 	private String description;
 
 	public String getBookName() {
-		return bookName;
+	return bookName;
 	}
 
 	public void setBookName(String bookName) {
-		this.bookName = bookName;
+	this.bookName = bookName;
 	}
 
 	public int getQuantity() {
-		return quantity;
+	return quantity;
 	}
 
 	public void setQuantity(int quantity) {
-		this.quantity = quantity;
+	this.quantity = quantity;
 	}
 
 	public Double getPrice() {
-		return price;
+	return price;
 	}
 
 	public void setPrice(Double price) {
-		this.price = price;
+	this.price = price;
 	}
 
 	public String getAuthorName() {
-		return this.authorName;
+	return this.authorName;
 	}
 
 	public void setAuthorName(String authorName) {
-		this.authorName = authorName;
+	this.authorName = authorName;
 	}
 
 	public String getimageURL() {
-		return imageURL;
+	return imageURL;
 	}
 
 	public void setimageURL(String imageURL) {
-		this.imageURL = imageURL;
+	this.imageURL = imageURL;
 	}
 
 	public String getDescription() {
-		return description;
+	return description;
 	}
 
 	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public BookDto(@NotBlank(message = "Book Name can not be empty") String bookName,
-			@Min(value = 1, message = "Quantity can't be less than 1") int quantity,
-			@Min(value = 0, message = "price can't be less than 0") Double price,
-			@NotBlank(message = "Author Name can not be empty") String authorName,
-			@NotBlank(message = "provide an image for book") String imageURL,
-			@NotBlank(message = "Book Description can not be empty") String description) {
-		super();
-		this.bookName = bookName;
-		this.quantity = quantity;
-		this.price = price;
-		this.authorName = authorName;
-		this.imageURL = imageURL;
-		this.description = description;
+	this.description = description;
 	}
 
 	@Override
 	public String toString() {
-		return "BookDto [bookName=" + bookName + ", quantity=" + quantity + ", price=" + price + ", authorName="
-				+ authorName + ", imageURL=" + imageURL + ", description=" + description + "]";
+	return "BookDto [bookName=" + bookName + ", quantity=" + quantity + ", price=" + price + ", authorName="
+	+ authorName + ", imageURL=" + imageURL + ", description=" + description + "]";
 	}
+	
 }
