c776c79fdccebbba588e7e148dbbcbde728f3e09
RameshAanji <rameshaanji97@gmail.com>
2020-07-16 10:33:26 +0530

    [Ramesh] Naming Convention

--- src/main/java/com/bridgelabz/bookstore/constants/Constant.java
+++ src/main/java/com/bridgelabz/bookstore/constants/Constant.java
@@ -69,11 +69,9 @@ public class Constant {
 	public static final String PROFILE_IMAGE_UPLOADED_FAILED = "Image Uploaded Failed";
 	public static final String USER_DETAILS_UPDATED_FAILED = "User Details Updated Failed";
 	public static final String RESET_PASSWORD = "http://localhost:4200/resetpassword/";
-
 	public static final String ADMIN_CREDENTIALS_MISMATCH = "Admin Credentials Mismatch";
 	public static final String USER_FOUND = "User Found";
 	public static final String BOOK_VERIFIED_SUCCESSFULLY_MEAASGE = "Book Verified Successfully";
-
 	public static final String UNAUTHORIZED_EXCEPTION_MESSAGE = "You are not Authorized";
 	public static final String APPROVAL_STATUS_WAITING = "waiting";
 	public static final String APPROVAL_STATUS_APPROVED = "approved";
@@ -97,47 +95,29 @@ public class Constant {
 	public static final String CART_ITEMS_LIMIT_EXCEEDED_MESSAGE = "Cart Books exceeded limit of 5";
 	public static final String CART_ITEM_LOW_LIMIT_MESSAGE = "Books In cart can't be less than one";
 	public static final String CART_EMPTY_MESSAGE = "No Books In cart To remove quantity";
-
 	public static final String BOOK_ADD_TO_WISHLIST = "book is succesfully added to the wishlist";
 	public static final String BOOK_ADD_TO_WISHLIST_FAILED = "failed to add to the wishlist";
-
-	public static final String BOOK_REMOVED_FROM_WISHLIST = "book is sucessfully removed from wishlist";
 	public static final String BOOK_REMOVAL_FROM_WISHLIST_FAILED = "failed to remove book from wishlist";
-
 	public static final String BOOKS_DISPLAY_MESSAGE = "Displaying items in wishlist";
 	public static final String BOOKS_DISPLAY_FAILED_MESSAGE = "Displaying items in wishlist failed";
-
 	public static final String ADDRESS_DETAILS_ADDED = "address details added succefully";
 	public static final String ADDRESS_DETAILS_FAIL = " address details failed to add ";
 	public static final String ADDRESS_DETAILS_FOUND = "address details found for required type succefully";
 	public static final String ADDRESS_DETAILS_NOT_fOUND = " not found address details for required type ";
-
 	public static final String ORDER_PLACED_SUCCESSFULLY = "Order placed Successfully";
-
 	public static final String ORDER_PLACED_FAILED = "Order placed Failed";
-
 	public static final String PLACE_ORDER_SUCCESSFUL_MESSAGE = "Order placed";
 	public static final String PLACE_ORDER_FAILED_MESSAGE = "Order placing Failed";
-
 	public static final String LIST_OF_ORDERS = "List of orders";
 	public static final String ORDER_EMPTY = "Order Empty";
-
 	public static final String USER_NOT_EXIST_PLEASE_REGISTER = "We cannot find an account with that Login credentials";
-
 	public static final String PLEASE_GIVE_CORRECT_PASSWORD = "Please Check Your Password";
-
 	public static final String YOUR_EMAIL_NOT_VERIFIED = "Your Email Not Verified";
-
 	public static final String USER_NOT_FOUND_WITH_ROLE = "User Not Exist With Corresponding Role";
-
 	public static final String QUANTITY_UPDATION_SUCCESS_MESSAGE = "Quantity Updation success";
-
 	public static final String QUANTITY_UPDATION_FAILED_MESSAGE = "Quantity Updation Failed";
-
 	public static final String CART_SIZE_FETCHED_SUCCESSFULLY = "Cart size fetched successfully";
-
 	public static final String CART_SIZE_FETCHING_FAILED = "Cart size fetched failed";
-
 	public static final int COUNT = 3;
 	
 
